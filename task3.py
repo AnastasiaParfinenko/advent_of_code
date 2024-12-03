18592f2bc1c93b7acb22a0eaabe80541c720c6d0
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-12-03 19:08:25 +0100

    Solve three tasks
    Solve third task with regular expression

--- task3.py
+++ task3.py
@@ -2,44 +2,16 @@ from os import write
 import re
 
 
-def check(line, cur_index):
-    c1, c2 = False, False
-    k = line.find(',', cur_index)
-    if k == -1:
-        return None
-    if k - cur_index <= 7 and line[cur_index + 4: k].isdigit():
-        num1 = int(line[cur_index + 4: k])
-        c1 = True
-    s = line.find(')', cur_index)
-    if s == -1:
-        return None
-    if s - k <= 4 and line[k + 1: s].isdigit():
-        num2 = int(line[k + 1: s])
-        c2 = True
-
-    if c1 and c2:
-        return num1 * num2
-    else:
-        return None
-
-def part1():
-    res = 0
-
+def  part1():
     with open('new_input.txt', 'r', encoding='utf-8') as file:
-        for line in file:
-            cur_index = -4
-            while True:
-                cur_index = line.find('mul(', cur_index + 4)
-                if cur_index == -1:
-                    break
-                if check(line, cur_index):
-                    res += check(line, cur_index)
-
-    return res
+        s = file.read()
+        pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+        all_mul = re.findall(pattern, s)
+        print(sum(int(mul[0]) * int(mul[1]) for mul in all_mul))
 
 
 def part2():
-    with open('input.txt', 'r', encoding='utf-8') as file:
+    with open('input3.txt', 'r', encoding='utf-8') as file:
         s = file.read()
         new_s = ''
         work = True
@@ -56,5 +28,5 @@ def part2():
         file.write(new_s)
 
 
-
-new_part1()
+part2()
+part1()
