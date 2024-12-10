ff15d4523e68b3fd83152698bcee2e2c8b71dba8
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-12-10 15:33:59 +0100

    Refactoring: move 'search_trails' to 'Grid'

--- task10.py
+++ task10.py
@@ -5,25 +5,24 @@ class Grid:
             self.size = len(self.data)
             self.nulls = [(i, j) for i in range(self.size) for j in range(self.size) if self.data[i][j] == '0']
 
+    def search_trails(self, path, trails=None):
+        if trails is None:
+            trails = []
 
-def search_trails(grid: Grid, path, trails=None):
-    if trails is None:
-        trails = []
+        if len(path) == 10:
+            trails.append(path)
+            return trails
 
-    if len(path) == 10:
-        trails.append(path)
+        i, j = path[-1][1]
+        for point in ((i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)):
+            if not all(0 <= point[i] < self.size for i in range(2)):
+                continue
+
+            num = int(self.data[point[0]][point[1]])
+            if num == path[-1][0] + 1:
+                self.search_trails(path + [(num, point)], trails)
+
         return trails
-
-    i, j = path[-1][1]
-    for point in ((i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)):
-        if not all(0 <= point[i] < grid.size for i in range(2)):
-            continue
-
-        num = int(grid.data[point[0]][point[1]])
-        if num == path[-1][0] + 1:
-            search_trails(grid, path + [(num, point)], trails)
-
-    return trails
 
 
 def count_uniq_tops(trails):
@@ -32,7 +31,7 @@ def count_uniq_tops(trails):
 
 def part12():
     grid = Grid()
-    trails = [search_trails(grid, [(0, null)]) for null in grid.nulls]
+    trails = [grid.search_trails([(0, null)]) for null in grid.nulls]
     uniq_tops = sum(count_uniq_tops(t) for t in trails)
     diversity_of_trails = sum(len(t) for t in trails)
 
