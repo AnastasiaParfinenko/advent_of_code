fffbda239f4b3d401edf1b709e606c87d164b2d5
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-12-11 14:39:40 +0100

    Reduce the number of lines

--- task11.py
+++ task11.py
@@ -7,26 +7,12 @@ def get_stones():
         return stones
 
 
-def one_blink(stones: list):
-    new_stones = []
-    for stone in stones:
-        if len(stone) % 2 == 0:
-            middle = len(stone) // 2
-            new_stones.append(stone[:middle])
-            new_stones.append(str(int(stone[middle:])))
-        elif stone == '0':
-            new_stones.append('1')
-        else:
-            new_stones.append(str(int(stone) * 2024))
-
-    return  new_stones
-
 @lru_cache(None)
-def count_stones(stone, time, cur_time):
+def count_stones(stone, total_blink, cur_blink):
     if not stone:
         return 0
 
-    if cur_time == time:
+    if cur_blink == total_blink:
         return 1
 
     if len(stone) % 2 == 0:
@@ -40,24 +26,13 @@ def count_stones(stone, time, cur_time):
         stone1 = str(int(stone) * 2024)
         stone2 = None
 
-    return count_stones(stone1, time, cur_time + 1) + count_stones(stone2, time, cur_time + 1)
+    return count_stones(stone1, total_blink, cur_blink + 1) + count_stones(stone2, total_blink, cur_blink + 1)
 
 
-def part1():
+def part12():
     stones = get_stones()
-    times = 25
-    for time in range(times):
-        stones = one_blink(stones)
-    print(len(stones))
+    for total_blink in [25, 75]:
+        print(sum(count_stones(stone, total_blink,0) for stone in stones))
 
 
-def part2():
-    stones = get_stones()
-    res = 0
-    for stone in stones:
-        res += count_stones(stone,75,0)
-    print(res)
-
-
-part1()
-part2()
+part12()
