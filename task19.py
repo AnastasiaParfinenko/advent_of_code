ed0cebb2d252e08b53b0caa6776f04124b05db9a
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-12-19 15:42:28 +0100

    Add solution to task 19 with two recursive functions for each part

--- task19.py
+++ task19.py
@@ -3,9 +3,9 @@ from functools import lru_cache
 
 def get_data():
     with open('input.txt', 'r') as file:
-        towels = tuple(file.readline().strip('\n').split(', '))
+        towels = tuple(file.readline().strip().split(', '))
         file.readline()
-        designs = [line.strip('\n') for line in file]
+        designs = [line.strip() for line in file]
 
     return towels, designs
 
@@ -13,21 +13,33 @@ def get_data():
 @lru_cache(None)
 def check(towels, design: str):
     if not design:
+        return True
+
+    for t in towels:
+        if design.startswith(t):
+            if check(towels, design[len(t):]):
+                return True
+
+    return False
+
+@lru_cache(None)
+def count_ways(towels, design: str):
+    if not design:
         return 1
 
     different_ways = 0
 
     for t in towels:
         if design.startswith(t):
-            different_ways += check(towels, design[len(t):])
+            different_ways += count_ways(towels, design[len(t):])
 
     return different_ways
 
 
 def part12():
     towels, designs = get_data()
-    ans1 = sum(1 for design in designs if check(towels, design))
-    ans2 = sum(check(towels, design) for design in designs)
+    ans1 = sum(check(towels, design) for design in designs)
+    ans2 = sum(count_ways(towels, design) for design in designs)
     print(ans1, ans2, sep='\n')
 
 
